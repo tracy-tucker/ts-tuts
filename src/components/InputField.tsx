f9040c7e5e0a5b16d2eb49ed1016b15df56fd8e0
Tracy Tucker <tracytuckeremail@gmail.com>
2022-05-11 08:58:05 -0500

    Setting up basic InpuField with todo, setTodo

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+// ANOTHER WAY TO WRITE THIS:
+// const InputField: React.FC<Props> = ({todo, setTodo})
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a Task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a Task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         GO
       </button>
