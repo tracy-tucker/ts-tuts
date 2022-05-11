9d1057558cf64f7285167b4fe28848fa5f559645
Tracy Tucker <tracytuckeremail@gmail.com>
2022-05-11 08:35:20 -0500

    Finishing button stylign of taskify

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./styles.css";
 
 const InputField = () => {
-  return <div>InputField</div>;
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a Task" className="input__box" />
+      <button className="input__submit" type="submit">
+        GO
+      </button>
+    </form>
+  );
 };
 
 export default InputField;
