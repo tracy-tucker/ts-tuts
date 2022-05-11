7f1e158de757a25ae2c63857179ad5f3b9981944
Tracy Tucker <tracytuckeremail@gmail.com>
2022-05-11 08:22:09 -0500

    Initial styling and empty InputField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
-function App() {
-  return <div className="App">Hello World.</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+
+      <InputField />
+    </div>
+  );
+};
 
 export default App;
 
