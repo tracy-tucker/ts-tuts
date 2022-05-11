0f952b0ec91ce005741cad9b8da372d50e55b013
Tracy Tucker <tracytuckeremail@gmail.com>
2022-05-11 11:40:20 -0500

    single todo comp iterating with styling

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Todo } from "./model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./styles.css";
 
-const SingleTodo: React.FC = () => {
-  return <div>SingleTodo</div>;
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  return (
+    <form className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
